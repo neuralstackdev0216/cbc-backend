cf1d293e86780eadfe3a4ab41c0d713896d58910
Dilshan0216 <neuralstack.dev@gmail.com>
2026-09-23 14:54:42 +0530

    Update Order(status)

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,5 +1,6 @@
 import Order from "../models/order.js"
 import Product from "../models/product.js";
+import { isAdmin } from "./userController.js";
 
 export async function createOrder(req,res){
     if(req.user==null){
@@ -120,3 +121,37 @@ export async function getOrders(req,res){
     }
 }
 
+export async function updateOrderStatus(req,res){
+    if(!isAdmin(req)){
+        res.status(403).json({
+            message:"You are not authorized to update order status"
+        })
+        return
+    }
+    try{
+        const orderId=req.params.orderId
+        const status=req.params.status
+
+        await Order.updateOne(
+            {
+                orderId:orderId
+            },
+            {
+                status:status
+            }
+        )
+        res.json(
+            {
+                message:"Order status updated successfully"
+            }
+        )
+    }catch(err){
+        res.status(500).json(
+            {
+                message:"Failed to update order status",
+                error:err
+            }
+        )
+        return
+    }
+}
